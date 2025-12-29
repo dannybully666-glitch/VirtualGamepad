a48fbea2d7319654b88a48e88d0bf48ff3342a08
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 12:14:52 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/virtualgamepad/MainActivity.kt
+++ app/src/main/java/com/example/virtualgamepad/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.example.virtualgamepad
 
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,29 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.startOverlay).setOnClickListener {
-            if (!Settings.canDrawOverlays(this)) {
-                startActivity(
-                    Intent(
-                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:$packageName")
-                    )
-                )
-                return@setOnClickListener
-            }
-
-            startOverlay()
-        }
-    }
-
-    private fun startOverlay() {
-        val intent = Intent(this, OverlayService::class.java)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent)
-        } else {
-            startService(intent)
-        }
     }
 }
