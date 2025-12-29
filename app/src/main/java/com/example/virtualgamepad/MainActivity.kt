b07de27d7359fd0c9934f7fa339955c4f361694b
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 11:01:02 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/virtualgamepad/MainActivity.kt
+++ app/src/main/java/com/example/virtualgamepad/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // No VirtualGamepad reference yet
+
+        val joystick = JoystickView(this)
+        setContentView(joystick)
     }
 }
