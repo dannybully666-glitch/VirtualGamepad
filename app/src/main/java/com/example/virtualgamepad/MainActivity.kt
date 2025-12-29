3ab7ba4ee8f5959018c23e1bf96b1a5c7d089778
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 11:30:37 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/virtualgamepad/MainActivity.kt
+++ app/src/main/java/com/example/virtualgamepad/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.virtualgamepad
 
-import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(JoystickView(this))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            !Settings.canDrawOverlays(this)
+        ) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        } else {
+            startService(Intent(this, OverlayService::class.java))
+            finish()
+        }
     }
 }
